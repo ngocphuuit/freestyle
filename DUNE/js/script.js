e34b4f1e9b0f3bf928d06a098137b24fb5cbca07
ngocphu <ngocphu.uit@gmail.com>
2014-08-15 11:43:41 +0700

    select

--- DUNE/js/script.js
+++ DUNE/js/script.js
@@ -1,92 +1,148 @@
 //window.onload = function(){
-  window.handleArrowKeys = function(e){
-  var e=window.event || e;
-  var clas = document.querySelectorAll(".forcus");
+var global_position = 0;
+
+window.onkeydown = function(evt) {
+    evt = evt || window.event;
+    //alert("keydown: " + evt.keyCode);
+
+    var li = document.querySelectorAll(".left_menu");
+    for (var i = 0; i < li.length; i++) {
+          var str = li[i].className;
+          var patt = /forcus/g;
+          var res = patt.test(str);
+      if(res){
+        var n = i;
+        break;
+      }
+    }
+
+  switch (evt.keyCode) {
+      case 37://left
+          if (global_position == 0) {
+            break;
+          } else{
+            app_leftright("left");
+          };    
+          break;
+
+      case 38: //up
+          if (global_position == 0) {
+            if (n != 0) {
+              menu("up",n);
+            };
+          } else{
+              app_updown("up");
+          };
+          break;   
+
+      case 39://right
+          if (global_position == 0) {
+            global_position = 1;
+            getalert();
+            break;
+          } else{
+            app_leftright("right");
+          }; 
+          break;
+      case 40: //down
+          if (global_position == 0) {
+            if (n != li.length-1) {
+              menu("down",n);
+            } 
+          } else {
+              app_updown("down");
+          };
+          break;
+
+      case 13: //enter
+          if (global_position == 0) {
+            global_position = 1;
+            getalert();
+          } else{
+            alert("dang o chanel list");
+          };
+          break;    
+    }
+
+};
+               
+
+function getalert(e){
+    var e=window.event || e;
+  var cl = document.querySelectorAll(".chanel");
+  cl[0].className += " forcus";
+}
+
+
+
+function menu(e,n){
   var li = document.querySelectorAll(".left_menu");
-  var m = li[2].querySelectorAll(".forcus");
-  for (var i = 0; i < li.length; i++) {
-        var str = li[i].className;
+  if (e == "up") {
+    li[n].setAttribute("class","left_menu");
+    n = n-1;
+    li[n].className += " forcus";
+  } else{
+    li[n].setAttribute("class","left_menu");
+    n = n+1;
+    li[n].className += " forcus";
+  };
+}
+
+
+// up down app
+function app_updown(e){
+  var li_chanel = document.querySelectorAll(".chanel");
+  for (var j = 0; j < li_chanel.length; j++) {
+        var str = li_chanel[j].className;
         var patt = /forcus/g;
         var res = patt.test(str);
     if(res){
-      var n = i;
+      var m = j;
       break;
-    }
-  }    //var top = getElementStyle("moveableElem", "top", "top");
-        //var left = getElementStyle("moveableElem", "left", "left");
-        //var elem = document.getElementById("moveableElem");
-        switch (e.keyCode) {
-            case 37: //left
-                //elem.style.left = (parseInt(left) - 5) + "px";
-                 //alert("trai");
-                 alert(clas);
-                break;    
-            case 38: //up
-                if (n == 0) {
-                  alert(n);
-                  break;
-                } else{
-                  //alert("sao vay");
-                  li[n].setAttribute("class","left_menu");
-                  n = n-1;
-                  li[n].className += " forcus";
-                };
-                //elem.style.top = (parseInt(top) - 5) + "px";
-                 //alert(li.length);
-                break;    
-            case 39: //right
-               // elem.style.left = (parseInt(left) + 5) + "px";
-                 alert(n);
-                break;    
-            case 40: //down
-                //elem.style.top = (parseInt(top) + 5) + "px";
-                 //alert(li[2].className);
-                 if (n == li.length-1) {
-                  alert(n);
-                  break;
-                } else{
-                  //alert("sao vay");
-                  li[n].setAttribute("class","left_menu");
-                  n = n+1;
-                  li[n].className += " forcus";
-                };
-                break;
-          }
-    }
-//}
-//setAttribute
+    };
+  }
+  if (e == "up") {
+    if (m > 2) {
+      li_chanel[m].setAttribute("class","chanel");
+      m = m-3;
+      li_chanel[m].className += " forcus";
+    };
+  } else{
+    if (m < 9) {
+      li_chanel[m].setAttribute("class","chanel");
+      m = m+3;
+      li_chanel[m].className += " forcus";
+    };
+  };
+}
 
-// function doSomething(e) {
-//     var code;
-//     if (!e) var e = window.event;
-//     if (e.keyCode) code = e.keyCode;
-//     else if (e.which) code = e.which;
-//     var character = String.fromCharCode(code);
-//     alert('Character was ' + character);
-// }
 
-// document.onKeyDown = function(event) {
-//      if (!event)
-//           event = window.event;
-//      var code = event.keyCode;
-//      if (event.charCode && code == 0)
-//           code = event.charCode;
-//      switch(code) {
-//           case 37:
-//               alert("trai");
-//               break;
-//           case 38:
-//               alert("trai");
-//               // Key up.
-//               break;
-//           case 39:
-//               alert("trai");
-//               // Key right.
-//               break;
-//           case 40:
-//               alert("trai");
-//               // Key down.
-//               break;
-//      }
-//      event.preventDefault();
-// };
+// left right app
+function app_leftright(e){
+  var li_chanel = document.querySelectorAll(".chanel");
+  for (var j = 0; j < li_chanel.length; j++) {
+        var str = li_chanel[j].className;
+        var patt = /forcus/g;
+        var res = patt.test(str);
+    if(res){
+      var m = j;
+      break;
+    };
+  }
+  if (e == "right") {
+    if (m != li_chanel.length-1 ) {
+      li_chanel[m].setAttribute("class","chanel");
+      m = m+1;
+      li_chanel[m].className += " forcus";
+    };
+  } else{
+    if (m == 0) {
+      global_position = 0;
+    } else{
+      li_chanel[m].setAttribute("class","chanel");
+      m = m-1;
+      li_chanel[m].className += " forcus";
+    };
+    
+  };
+}
