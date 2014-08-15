762464f73ddfa2933820303ea6bd8333b421a592
Nopu Nguyen <ngocphu.uit@gmail.com>
2014-08-15 07:33:30 +0700

    javascript

--- DUNE/js/script.js
+++ DUNE/js/script.js
@@ -1,29 +1,39 @@
-// function handleArrowKeys(evt) {
-//     evt = (evt) ? evt : ((window.event) ? event : null);
-//     if (evt) {
-//         var top = getElementStyle("moveableElem", "top", "top");
-//         var left = getElementStyle("moveableElem", "left", "left");
-//         var elem = document.getElementById("moveableElem");
-//         switch (evt.keyCode) {
-//             case 37:
-//                 elem.style.left = (parseInt(left) - 5) + "px";
-//                  alert("trai");
-//                 break;    
-//             case 38:
-//                 elem.style.top = (parseInt(top) - 5) + "px";
-//                  alert("phai");
-//                 break;    
-//             case 39:
-//                 elem.style.left = (parseInt(left) + 5) + "px";
-//                  alert("left");
-//                 break;    
-//             case 40:
-//                 elem.style.top = (parseInt(top) + 5) + "px";
-//                  alert("top");
-//                 break;    
-//          }
-//     }
-// }
+window.onload = function(){
+  var li = document.querySelectorAll(".left_menu");
+  li[2].className += " forcus";
+}
+//window.onload = function(){
+  window.handleArrowKeys = function(e){
+  var e=window.event || e;
+  var clas = document.querySelectorAll(".forcus");
+  var li = document.querySelectorAll(".left_menu");
+  var m = document.li[2].querySelectorAll(".forcus");
+  for (var i = 0; i < li.length; i++) {
+  }    //var top = getElementStyle("moveableElem", "top", "top");
+        //var left = getElementStyle("moveableElem", "left", "left");
+        //var elem = document.getElementById("moveableElem");
+        switch (e.keyCode) {
+            case 37: //left
+                //elem.style.left = (parseInt(left) - 5) + "px";
+                 //alert("trai");
+                 alert(clas);
+                break;    
+            case 38: //up
+                //elem.style.top = (parseInt(top) - 5) + "px";
+                 alert(li.length);
+                break;    
+            case 39: //right
+               // elem.style.left = (parseInt(left) + 5) + "px";
+                 alert(m.length);
+                break;    
+            case 40: //down
+                //elem.style.top = (parseInt(top) + 5) + "px";
+                 alert("top");
+                break;
+          }
+    }
+//}
+//setAttribute
 
 // function doSomething(e) {
 //     var code;
@@ -34,28 +44,28 @@
 //     alert('Character was ' + character);
 // }
 
-document.onKeyDown = function(event) {
-     if (!event)
-          event = window.event;
-     var code = event.keyCode;
-     if (event.charCode && code == 0)
-          code = event.charCode;
-     switch(code) {
-          case 37:
-              alert("trai");
-              break;
-          case 38:
-              alert("trai");
-              // Key up.
-              break;
-          case 39:
-              alert("trai");
-              // Key right.
-              break;
-          case 40:
-              alert("trai");
-              // Key down.
-              break;
-     }
-     event.preventDefault();
-};
+// document.onKeyDown = function(event) {
+//      if (!event)
+//           event = window.event;
+//      var code = event.keyCode;
+//      if (event.charCode && code == 0)
+//           code = event.charCode;
+//      switch(code) {
+//           case 37:
+//               alert("trai");
+//               break;
+//           case 38:
+//               alert("trai");
+//               // Key up.
+//               break;
+//           case 39:
+//               alert("trai");
+//               // Key right.
+//               break;
+//           case 40:
+//               alert("trai");
+//               // Key down.
+//               break;
+//      }
+//      event.preventDefault();
+// };
